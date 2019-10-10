0976165aca98977b4dd24e4f76a71d2a45e30f80
Derek <derekluu90@gmail.com>
2019-10-09 23:23:22 -0400

    working fetch and metascrape

--- src/metascrapper/metascrapper.js
+++ src/metascrapper/metascrapper.js
@@ -12,9 +12,9 @@ const Qs = require('qs')
 const proxyURL = 'https://proxy.hackeryou.com';
 
 
-const targetUrl = 'https://genevievefiles.blogspot.com/2019/10/what-to-do-what-to-do-privacy-morality.html'; 
+// const targetUrl = 'https://genevievefiles.blogspot.com/2019/10/what-to-do-what-to-do-privacy-morality.html'; 
 
-export const metascrape = async () => {
+export const metascrape = async (targetUrl) => {
     // const { body: html, url } = await got(targetUrl)
 
     const response = await axios({
@@ -27,10 +27,9 @@ export const metascrape = async () => {
             reqUrl: targetUrl,
         }
     })
-    
     const html  = response.data
     const url = response.config.params.reqUrl 
 
     const metadata = await metascraper({ html, url })
     console.log(metadata)
-})
+}
