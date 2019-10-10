d7f278452471d6b5931ef03a6e75cf1bfbb01ef1
Derek <derekluu90@gmail.com>
2019-10-10 14:38:26 -0400

    merge stash

--- src/Components/Articles.js
+++ src/Components/Articles.js
@@ -3,29 +3,31 @@ import { metascrape } from '../metascrapper/metascrapper'
 
 const Articles = () => {
 
-    const [articles, setArticles] = useState([]);
+    const [articleUrls, setArticleUrls] = useState([]);
+    const [articlesMeta, setMeta] = useState([]);
 
-    const getArticles = () => {
-        fetch('https://hacker-news.firebaseio.com/v0/newstories.json?&print=pretty')
-            .then(response => response.json())
-            .then(artId => {
-                artId.forEach(id => {
-                    fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?&print=pretty`)
-                    .then(res => res.json())
-                    .then(art => {
-                        if(art){
-                            setArticles(prevArticles => [...prevArticles, art.url])
-                        }
-                    })
-                })
-            })
+    const getArticleMeta = (articles) => {
+        articles.forEach(metascrape)
     }
 
     useEffect(() => {
-        getArticles();
-        metascrape('https://medium.com/@hubret/basic-metascraper-for-lists-of-urls-aa8f8636f344')
-    }, []) //without this argument, useEffect would call getArticles every time setArticles is called
+        const getArticlesUrl = async () => {
+            const promiseId = await fetch('https://hacker-news.firebaseio.com/v0/newstories.json?&print=pretty');
+            const articleIds = await promiseId.json();
+    
+            const promiseArticles = articleIds.map(id => {
+                return fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?&print=pretty`)
+                    .then(res => res.json())
+            });
+            
+            const articleData = await Promise.all(promiseArticles);
+            const articleUrl = articleData.map(async (article) => article && article) //if object exists, add it to array
+            Promise.all(articleUrl).then(setArticleUrls)
+        }
 
+        getArticlesUrl();
+
+    },[]) //without the empty array argument, useEffect would call getArticles every time setArticles is called
 
 
     // have a filter that is always removing the first 30 from the results
@@ -35,12 +37,20 @@ const Articles = () => {
     // unless this is just a trick (dom manipulation) to hide information that is already available
     return (
         <div>
-            {
-                articles.map(url => (
-                    <p>{url}</p>
-                ))
-            }
-            <h2>Articles</h2>
+        {
+            articleUrls.length > 0 && getArticleMeta(articleUrls)
+        }
+        <h2>Articles</h2>
+        {
+            articleUrls.length > 0 ? 
+            articleUrls.map(art => (
+                <div>
+                    <p>{art.url}</p>
+                </div>
+            )) 
+            :
+            <p>Loading...</p>
+        }
         </div>
     )
 }
