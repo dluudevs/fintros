c0a5411515bdb8bcac548b689c6752c96cabfeb3
Derek <derekluu90@gmail.com>
2019-10-15 19:11:43 -0400

    load more

--- src/Components/Articles.js
+++ src/Components/Articles.js
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { metascrape } from '../metascrapper/metascrapper';
+import InfiniteScroll from 'react-infinite-scroller';
 
 const Articles = () => {
 
     const [meta, setMeta] = useState([]);
     const [artId, setArtId] = useState([]);
 
-    const showArticles = (articles) => {
+    const loader = (
+        <div className="loading font_grey">
+            <p>Fetching Articles ... </p>
+        </div>
+    )
 
-        console.log(articles)
-        
+    const showArticles = (articles = meta) => {
+
         const results = articles.map(({ description, title, image, id, url }, index) => {
             return (
                 <div key={id} data-id={index} className="article">
@@ -27,8 +32,10 @@ const Articles = () => {
         return results;
     }
 
-    const getArticleMeta = async (articles) => {
+    const getArticleMeta = async (articles = artId) => {
 
+        console.log('getArticleMeta called')
+        
         const results = [];
         let index = 0; 
         
@@ -53,13 +60,12 @@ const Articles = () => {
             }
         }
 
-        while(results.length < 29){ 
+        while(results.length < 30){ 
             await fetchArticles(articles[index]); //wait for loop to complete 
         }
 
-
-        setArtId(oldId => oldId.slice(0, index)) //remove items that were search from articleId
-        setMeta(oldMeta => [...oldMeta, ...results]);
+        setArtId(oldId => oldId.splice(index, oldId.length)) //remove items that were search from articleId
+        setMeta(oldMeta => [...oldMeta, ...results.splice(0, 30)]);
 
     }
 
@@ -77,20 +83,34 @@ const Articles = () => {
 
     }, [])
 
+    useEffect(() => {
+        console.log(artId)
+    }, [artId])
+
+    useEffect(() => {
+        console.log('meta updated')
+    }, [meta])
+
     return (
         <section className="wrapper">
             <div className="article_container">
-                {
-                    meta.length ? showArticles(meta) : 
-                    (
-                        <div className="loading font_grey">
-                            <p>Fetching Articles ... </p>
-                        </div>
-                    )
-                }
+                {meta.length > 0 ? showArticles() : loader}
+                <button onClick={() => getArticleMeta()}>Load More</button>
             </div>
         </section>
     )
 }
 
 export default Articles;
+
+// <InfiniteScroll
+//     pageStart={0}
+//     loadMore={getArticleMeta}
+//     hasMore={true}
+//     loader={loader}
+//     key={'is1'}
+// >
+//     {
+//         showArticles(meta)
+//     }
+// </InfiniteScroll>
